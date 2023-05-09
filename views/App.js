3ea2858df4f8a221ae92d5b528ff1b30ca068e60
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-05-09 11:21:37 -0700

    .

---
+++ views/App.js
@@ -0,0 +1,10 @@
+const tiktokStreams = [
+    {
+      profileImage: "https://th.bing.com/th?id=OIP.IDLD7-6hTh68NAOzJeq60wHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+      title: "Bryce Hall",
+    },
+    {
+      profileImage: "https://th.bing.com/th?id=OIP.IDLD7-6hTh68NAOzJeq60wHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+      title: "Addison Rae",
+    },
+  ];
