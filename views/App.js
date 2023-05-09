05e1431145cb583e3f28a843bc45d1f6248b3ed3
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-05-09 11:25:10 -0700

    .

--- views/App.js
+++ views/App.js
@@ -8,3 +8,16 @@ const tiktokStreams = [
       title: "Addison Rae",
     },
   ];
+
+  const youtubeStreams = [
+    {
+      profileImage: "https://th.bing.com/th?id=OIP.aByvAl4Fl4Dk3N9fA3IudwHaFM&w=298&h=209&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+      title: "Mr. Beast",
+    },
+    {
+      profileImage: "https://th.bing.com/th?id=OIP.aByvAl4Fl4Dk3N9fA3IudwHaFM&w=298&h=209&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+      title: "PewDiePie",
+    },
+  ];
+  
+  
