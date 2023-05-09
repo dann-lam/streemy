5b5618b51605fe0d15640bcb318f7aa42f992edf
Daniel Lam <Dann.lam@gmail.com>
2023-05-09 14:05:18 -0700

    finished models

--- models/index.js
+++ models/index.js
@@ -3,33 +3,10 @@ const Streamer = require("./Streamer");
 const Platform = require("./Platform");
 
 User.belongsToMany(Streamer, { through: "User_Streamer" });
+Streamer.belongsToMany(User, { through: "User_Streamer" });
 
-Streamer.belongsTo(User);
-// User.hasMany(Streamer, {
-//   foreignKey: "user_id",
-//   onDelete: "CASCADE",
-// });
-
-// User.hasMany(CPlatform, {
-//   foreignKey: "user_id",
-//   onDelete: "CASCADE",
-// });
-
-// Streamer.belongsTo(User, {
-//   foreignKey: "user_id",
-// });
-
-// Streamer.hasMany(Platform, {
-//   foreignKey: "post_id",
-//   onDelete: "CASCADE",
-// });
-
-// Platform.belongsTo(User, {
-//   foreignKey: "user_id",
-// });
-
-// Platform.belongsTo(Streamer, {
-//   foreignKey: "post_id",
-// });
+Platform.belongsTo(Streamer, {
+  foreignKey: "platform_id",
+});
 
 module.exports = { User, Streamer, Platform };
