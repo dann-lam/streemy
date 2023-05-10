6c2683e74076598d60764ddb24fe631eae049850
Daniel Lam <Dann.lam@gmail.com>
2023-05-10 11:11:49 -0700

    new route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -6,41 +6,47 @@ router.post("/signup", async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
     //Get the username.
-    let password = req.body.password;
-    let email = req.body.email;
+    let passwordInput = req.body.password;
+    let emailInput = req.body.email;
     //We should check to see if the user is already in the database already
     //If they are, then we can send a message saying that person already exists.
     //Otherwise register the user.
     const userData = await User.findOne({
-      where: { username: req.body.username },
+      where: { email: emailInput },
     });
-
+    //If you cannot find on signup... that means there is no user with that email. Which means we can sign them up.
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
+      // const dbUserData =
+      User.create({
+        email: emailInput,
+        password: passwordInput,
+      });
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
 
-    // Verify the posted password with the password store in the database
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
+        // Redirect to index.html upon successful signup
+        res.redirect("/index.html");
+      });
     }
 
     // Create session variables based on the logged in user
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json({ user: userData, message: "You are now logged in!" });
-    });
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+// CREATE new user
+router.post("/", async (req, res) => {
+  try {
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
