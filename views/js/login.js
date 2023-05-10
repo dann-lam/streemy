b470fa03ed7c4e5578dcacd5cf50e29d3f5f9ae2
Ss0110 <saraysanchez01@yahoo.com>
2023-05-10 02:06:58 -0700

    added to js file

--- views/js/login.js
+++ views/js/login.js
@@ -1,2 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const showLoginFormButton = document.getElementById("show-login-btn");
+    const loginForm = document.getElementById("login-form");
+    const showSignupFormButton = document.getElementById("show-signup-btn");
+    const signupForm = document.getElementById("signup-form");
+  
+    showLoginFormButton.addEventListener("click", function() {
+      loginForm.style.display = "block";
+      signupForm.style.display = "none";
+    });
+  
+    showSignupFormButton.addEventListener("click", function() {
+      signupForm.style.display = "block";
+      loginForm.style.display = "none";
+    });
+  });
 
+  const emailInput = document.getElementById("email");
+
+  emailInput.addEventListener("input", function() {
+    if (!emailInput.checkValidity()) {
+      emailInput.setCustomValidity("Please enter a valid email address.");
+    } else {
+      emailInput.setCustomValidity("");
+    }
+  });
+  
   
