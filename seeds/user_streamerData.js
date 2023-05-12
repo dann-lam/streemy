c0cde9147a611e9192d3dd802510a6ef4cf56227
Ss0110 <saraysanchez01@yahoo.com>
2023-05-11 17:45:28 -0700

    made changes to user_streamerData

--- seeds/user_streamerData.js
+++ seeds/user_streamerData.js
@@ -32,6 +32,7 @@ const user_streamerData = [
   {
     user_id: 2,
     streamer_id: 2,
+    favorited: true,
   },
   {
     user_id: 2,
@@ -40,10 +41,12 @@ const user_streamerData = [
   {
     user_id: 2,
     streamer_id: 8,
+    favorited: true,
   },
   {
     user_id: 3,
     streamer_id: 2,
+    favorited: true,
   },
   {
     user_id: 3,
@@ -56,10 +59,12 @@ const user_streamerData = [
   {
     user_id: 3,
     streamer_id: 6,
+    favorited: true,
   },
   {
     user_id: 4,
     streamer_id: 3,
+    favorited: true,
   },
   {
     user_id: 4,
@@ -72,6 +77,7 @@ const user_streamerData = [
   {
     user_id: 4,
     streamer_id: 8,
+    favorited: true,
   },
   {
     user_id: 5,
@@ -84,18 +90,22 @@ const user_streamerData = [
   {
     user_id: 5,
     streamer_id: 4,
+    favorited: true,
   },
   {
     user_id: 5,
     streamer_id: 9,
+    favorited: true,
   },
   {
     user_id: 6,
     streamer_id: 4,
+    favorited: true,
   },
   {
     user_id: 6,
     streamer_id: 6,
+    favorited: true,
   },
   {
     user_id: 6,
